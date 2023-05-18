36f6f38369547e8d8efaa6aa8f87a405b488c6a0
sudarshankapali <sudarshankapalisk@gmail.com>
2023-05-18 23:31:53 +0545

    initial commit

---
+++ TicTacToe/src/TicTacToe.java
@@ -0,0 +1,163 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class TicTacToe implements ActionListener {
+    Random random = new Random();
+    JFrame jFrame = new JFrame("TicTacToe");
+    JPanel title_Panel = new JPanel();
+    JPanel button_Panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean player1_true;
+    TicTacToe(){
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setSize(800,800);
+        jFrame.getContentPane().setBackground(new Color(50,50,50));
+        jFrame.setLayout(new BorderLayout());
+        jFrame.setVisible(true);
+
+        textfield.setBackground(new Color(25,25,25));
+        textfield.setForeground(new Color(25,255,0));
+        textfield.setFont(new Font("Ink Free",Font.BOLD,75));
+        textfield.setHorizontalAlignment(JLabel.CENTER);
+        textfield.setText("Tic-Tac-Toe");
+        textfield.setOpaque(true);
+
+        title_Panel.setLayout(new BorderLayout());
+        title_Panel.setBounds(0,0,800,100);
+
+        button_Panel.setLayout(new GridLayout(3,3));
+        textfield.setBackground(new Color(125,125,125));
+
+        for(int i=0;i<9;i++){
+            buttons[i] = new JButton();
+            button_Panel.add(buttons[i]);
+            buttons[i].setFont(new Font("MV Boil",Font.BOLD,120));
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(this::actionPerformed);
+        }
+
+        title_Panel.add(textfield);
+        jFrame.add(title_Panel,BorderLayout.NORTH);
+
+        jFrame.add(button_Panel);
+
+        firstTurn();
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for(int i=0;i<9;i++){
+            if(e.getSource()==buttons[i]){
+                if(player1_true){
+                    if(buttons[i].getText()==""){
+                        buttons[i].setForeground(new Color(255,0,0));
+                        buttons[i].setText("X");
+                        player1_true = false;
+                        textfield.setText("O turn");
+                        check();
+                    }
+                }
+                else{
+                    if(buttons[i].getText()==""){
+                        buttons[i].setForeground(new Color(255,0,0));
+                        buttons[i].setText("O");
+                        player1_true = true;
+                        textfield.setText("X turn");
+                        check();
+                    }
+                }
+            }
+        }
+    }
+
+    public void firstTurn(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        if(random.nextInt(2)==0){
+            player1_true = true;
+            textfield.setText("X turn");
+        }
+        else{
+            player1_true = false;
+            textfield.setText("O turn");
+        }
+    }
+    public void check(){
+        if((buttons[0].getText()=="X")&&(buttons[1].getText()=="X")&&(buttons[2].getText()=="X")){
+            xWins(0,1,2);
+        }
+        if((buttons[3].getText()=="X")&&(buttons[4].getText()=="X")&&(buttons[5].getText()=="X")){
+            xWins(3,4,5);
+        }
+        if((buttons[6].getText()=="X")&&(buttons[7].getText()=="X")&&(buttons[8].getText()=="X")){
+            xWins(6,7,8);
+        }
+        if((buttons[0].getText()=="X")&&(buttons[3].getText()=="X")&&(buttons[6].getText()=="X")){
+            xWins(0,3,6);
+        }
+        if((buttons[1].getText()=="X")&&(buttons[4].getText()=="X")&&(buttons[7].getText()=="X")){
+            xWins(1,4,7);
+        }
+        if((buttons[2].getText()=="X")&&(buttons[5].getText()=="X")&&(buttons[8].getText()=="X")){
+            xWins(2,5,8);
+        }
+        if((buttons[0].getText()=="X")&&(buttons[4].getText()=="X")&&(buttons[8].getText()=="X")){
+            xWins(0,4,8);
+        }
+        if((buttons[2].getText()=="X")&&(buttons[4].getText()=="X")&&(buttons[6].getText()=="X")){
+            xWins(2,4,6);
+        }
+
+        //o wins
+        if((buttons[0].getText()=="O")&&(buttons[1].getText()=="O")&&(buttons[2].getText()=="O")){
+            oWins(0,1,2);
+        }
+        if((buttons[3].getText()=="O")&&(buttons[4].getText()=="O")&&(buttons[5].getText()=="O")){
+            oWins(3,4,5);
+        }
+        if((buttons[6].getText()=="O")&&(buttons[7].getText()=="O")&&(buttons[8].getText()=="O")){
+            oWins(6,7,8);
+        }
+        if((buttons[0].getText()=="O")&&(buttons[3].getText()=="O")&&(buttons[6].getText()=="O")){
+            oWins(0,3,6);
+        }
+        if((buttons[1].getText()=="O")&&(buttons[4].getText()=="O")&&(buttons[7].getText()=="O")){
+            oWins(1,4,7);
+        }
+        if((buttons[2].getText()=="O")&&(buttons[5].getText()=="O")&&(buttons[8].getText()=="O")){
+            oWins(2,5,8);
+        }
+        if((buttons[0].getText()=="O")&&(buttons[4].getText()=="O")&&(buttons[8].getText()=="O")){
+            oWins(0,4,8);
+        }
+        if((buttons[2].getText()=="O")&&(buttons[4].getText()=="O")&&(buttons[6].getText()=="O")){
+            oWins(2,4,6);
+        }
+    }
+    public void xWins(int a,int b,int c){
+        buttons[a].setBackground(Color.GREEN);
+        buttons[b].setBackground(Color.GREEN);
+        buttons[c].setBackground(Color.GREEN);
+
+        for(int i=0;i<9;i++){
+            buttons[i].setEnabled(false);
+        }
+        textfield.setText("X WINS");
+    }
+    public void oWins(int a,int b,int c){
+        buttons[a].setBackground(Color.GREEN);
+        buttons[b].setBackground(Color.GREEN);
+        buttons[c].setBackground(Color.GREEN);
+
+        for(int i=0;i<9;i++){
+            buttons[i].setEnabled(false);
+        }
+        textfield.setText("O WINS");
+    }
+}
